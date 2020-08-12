926dfb4f0f6a218da94fc8a85e30eb3b0bc80826
jayblacc2 <jideman2k@yandex.ru>
2020-08-12 13:04:23 +0400

    Sound Muse first commit

---
+++ dist/js/index.js
@@ -0,0 +1,8 @@
+let textShow = document.querySelector(".btn-wrap .text-muted");
+console.log(textShow);
+
+textShow.addEventListener("click", function (e) {
+  if (className == "text-muted") {
+    textShow.toggle(show);
+  }
+});
